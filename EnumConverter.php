52e3a86ea3d7bb89daa579262cd59df317ccdc8c
mdmunir <misbahuldmunir@gmail.com>
2014-11-14 08:06:35 +0700

    bug enum converter

--- EnumConverter.php
+++ EnumConverter.php
@@ -47,17 +47,17 @@ class EnumConverter extends BaseConverter
             return $this->enum[$value];
         }
         $className = get_class($this->owner);
-        if (!isset(static::$_constants[$className])) {
+        if (!isset(static::$_constants[$className][$this->enumPrefix])) {
             $ref = new ReflectionClass($className);
-            static::$_constants[$className] = [];
+            static::$_constants[$className][$this->enumPrefix] = [];
             foreach ($ref->getConstants() as $constName => $constValue) {
                 if ($this->enumPrefix === '' || strpos($constName, $this->enumPrefix) === 0) {
-                    static::$_constants[$className][$constValue] = substr($constName, strlen($this->enumPrefix));
+                    static::$_constants[$className][$this->enumPrefix][$constValue] = substr($constName, strlen($this->enumPrefix));
                 }
             }
         }
 
-        $str = isset(static::$_constants[$className][$value]) ? static::$_constants[$className][$value] : '';
+        $str = isset(static::$_constants[$className][$this->enumPrefix][$value]) ? static::$_constants[$className][$this->enumPrefix][$value] : '';
 
         return $this->toWord ? Inflector::camel2words(strtolower($str)) : $str;
     }
